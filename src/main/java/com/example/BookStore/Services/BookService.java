bf459b0585f74a63de9bc958ceb9001a46cf7261
Chaitanya_Anchal <chaitanyaanchal3107@gmail.com>
2025-12-31 02:55:15 +0530

    created book service interface along with its implementation to create, get update and delete book from database

---
+++ src/main/java/com/example/BookStore/Services/BookService.java
@@ -0,0 +1,20 @@
+package com.example.BookStore.Services;
+
+import com.example.BookStore.Models.Book;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BookService {
+
+    public Mono<Book> create(Book book);
+
+    public Flux<Book> getAll();
+
+    public Mono<Book> get(Integer bookId);
+
+    public Mono<Book> update(Book book,Integer bookId );
+
+    public Mono<Void> delete(Integer bookId);
+
+    public Flux<Book> search(String query);
+}
---
+++ src/main/java/com/example/BookStore/Services/impl/BookServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.BookStore.Services.impl;
+
+import com.example.BookStore.Models.Book;
+import com.example.BookStore.Repository.BookRepository;
+import com.example.BookStore.Services.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public Mono<Book> create(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public Flux<Book> getAll() {
+        return bookRepository.findAll().delayElements(Duration.ofSeconds(2));
+    }
+
+    @Override
+    public Mono<Book> get(Integer bookId) {
+        bookRepository.findById(bookId);
+        return null;
+    }
+
+    @Override
+    public Mono<Book> update(Book book, Integer bookId) {
+        Mono<Book> oldBook = bookRepository.findById(bookId);
+        return oldBook.flatMap(newBook -> {
+            newBook.setName(book.getName());
+            newBook.setAuthor(book.getAuthor());
+            newBook.setPublisher(book.getPublisher());
+            newBook.setDescription(book.getDescription());
+            return bookRepository.save(newBook);
+        });
+    }
+
+    @Override
+    public Mono<Void> delete(Integer bookId) {
+        return bookRepository.findById(bookId).flatMap(bookRepository::delete);
+    }
+
+    @Override
+    public Flux<Book> search(String query) {
+        return null;
+    }
+}
